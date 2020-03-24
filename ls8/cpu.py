78d1abbe28373cda30b9328bfa0c2c4d44b117e5
Dom Eccleston <domeccleston@gmail.com>
2020-03-24 15:10:32 +0000

    branch table working

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,6 +10,44 @@ class CPU:
         self.pc = 0 # initialize program counter to 0
         self.reg = [0] * 8
         self.ram = [0] * 256
+
+        self.instructions = {
+            1: "HLT",
+            130: "LDI",
+            71: "PRN",
+            162: "MUL"
+        }
+
+        self.branch_table = {
+            0: self._pass,
+            1: self.halt,
+            130: self.load_immediate,
+            71: self._print,
+            162: self.mult
+        }
+
+    def _pass(self, instruction, operand_a, operand_b):
+        self.pc += 1
+        pass
+
+    def halt(self, instruction, operand_a, operand_b):
+        halted = True
+        sys.exit(1)
+
+    def load_immediate(self, instruction, operand_a, operand_b):
+        register = operand_a
+        integer = operand_b
+        self.reg[register] = integer
+        self.pc += 3
+
+    def _print(self, instruction, operand_a, operand_b):
+        register = operand_a
+        print(self.reg[register])
+        self.pc += 1
+
+    def mult(self, instruction, operand_a, operand_b):
+        self.alu('MUL', operand_a, operand_b)
+        self.pc += 3
     
     def ram_read(self, address):
         return self.ram[address]
@@ -72,61 +110,12 @@ class CPU:
         """Run the CPU."""
 
         halted = False
-
-        HLT = 1
-        LDI = 130
-        PRN = 71 
-        MUL = 162
-
-        def halt():
-            halted = True
-            sys.exit(1)
-
-        def load_immediate(operand_a, operand_b):
-            register = operand_a
-            integer = operand_b
-            self.reg[register] = integer
-            self.pc += 3
-
-        def _print():
-            register = operand_a
-            print(self.reg[register])
-            self.pc += 1
-
-        def mult(operand_a, operand_b):
-            self.alu('MUL', operand_a, operand_b)
-            self.pc += 3
-
-
-        branch_table = {
-            1: halt(),
-            130: load_immediate(),
-            71: _print(),
-            162: mult(),
-        }
-
+        
         while not halted:
 
             instruction = self.ram_read(self.pc)
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
-            if instruction == HLT:
-                halted = True
-                break
-
-            elif instruction == MUL:
-                self.alu('MUL', operand_a, operand_b)
-                self.pc += 3
-            
-            elif instruction == LDI:    
-                register = operand_a
-                integer = operand_b
-                self.reg[register] = integer
-                self.pc += 3
-
-            elif instruction == PRN:
-                register = operand_a
-                print(self.reg[register])
-                self.pc += 1
+            self.branch_table[instruction](instruction, operand_a, operand_b)
 
